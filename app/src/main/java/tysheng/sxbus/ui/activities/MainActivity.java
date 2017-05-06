8c740a226921c8a5f7cd441e72f35f91c125613e
tysheng <353491983@qq.com>
2017-05-06 15:39:34 +0800

    3.3

--- app/src/main/java/tysheng/sxbus/ui/activities/MainActivity.java
+++ app/src/main/java/tysheng/sxbus/ui/activities/MainActivity.java
@@ -1,8 +1,18 @@
 package tysheng.sxbus.ui.activities;
 
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+
+import java.util.Arrays;
 
 import tysheng.sxbus.R;
 import tysheng.sxbus.base.BaseActivityV2;
@@ -14,6 +24,8 @@ import tysheng.sxbus.ui.inter.MainView;
 import tysheng.sxbus.view.PositionBottomNavigationView;
 
 public class MainActivity extends BaseActivityV2<MainPresenterImpl, ActivityMainBinding> implements MainView, PositionBottomNavigationView.onPositionSelectedListener, BaseFragmentV2.FragmentCallback {
+    private int shortcutId;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
@@ -28,6 +40,32 @@ public class MainActivity extends BaseActivityV2<MainPresenterImpl, ActivityMain
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_Main);
         super.onCreate(savedInstanceState);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
+            ShortcutManager shortcutManager;
+            shortcutManager = getSystemService(ShortcutManager.class);
+            try {
+                ShortcutInfo shortcutInfo = getShortcutInfo(1, "shortcut_query", R.mipmap.shortcut_search, "查询");
+                ShortcutInfo shortcutInfo2 = getShortcutInfo(2, "shortcut_more", R.mipmap.shortcut_menu, "更多");
+                shortcutManager.removeAllDynamicShortcuts();
+                shortcutManager.setDynamicShortcuts(Arrays.asList(shortcutInfo, shortcutInfo2));
+            } catch (IllegalStateException ignored) {
+
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+    @NonNull
+    private ShortcutInfo getShortcutInfo(int pos, String shortcutId, int iconRes, String label) {
+        Intent intent = new Intent(Intent.ACTION_MAIN, Uri.EMPTY, this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra("shortcut", pos);
+        Icon icon = Icon.createWithResource(this, iconRes);
+        return new ShortcutInfo.Builder(this, shortcutId)
+                .setShortLabel(label)
+                .setLongLabel(label)
+                .setIcon(icon)
+                .setIntent(intent)
+                .build();
     }
 
     @Override
@@ -42,6 +80,25 @@ public class MainActivity extends BaseActivityV2<MainPresenterImpl, ActivityMain
         mPresenter.restorePosition(savedInstanceState);
         binding.bottom.registerIds(R.id.menu_star, R.id.menu_search, R.id.menu_more);
         binding.bottom.setOnPositionSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            int pos = getIntent().getIntExtra("shortcut", 0);
+            if (pos == 1) {
+                shortcutId = R.id.menu_search;
+            } else if (pos == 2) {
+                shortcutId = R.id.menu_more;
+            }
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (shortcutId != 0) {
+            binding.bottom.setSelectedItemId(shortcutId);
+            shortcutId = 0;
+        }
     }
 
     @Override
